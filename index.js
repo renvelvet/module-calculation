d77e12cb2943557227c6a062cc99e9908c7794b3
Resha Puspita <reshapuspita734@gmail.com>
2020-06-19 14:51:52 +0700

    add string di console

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 import { add, subs, mul, div } from "./module.js";
 
-console.log(add(5, 5));
-console.log(subs(5, 5));
-console.log(mul(5, 5));
-console.log(div(5, 5));
+let a = 5;
+
+console.log(`${a} + ${a} = ${add(5, 5)}`);
+console.log(`${a} + ${a} = ${subs(5, 5)}`);
+console.log(`${a} + ${a} = ${mul(5, 5)}`);
+console.log(`${a} + ${a} = ${div(5, 5)}`);
